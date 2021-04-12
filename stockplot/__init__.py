2c04c47b76e62a178053564972b2b0c8b96afa9e
tom <tom@tomlaeven.com>
2021-04-12 14:48:43 +0200

    Remove from package import *

--- stockplot/__init__.py
+++ stockplot/__init__.py
@@ -1,2 +1,4 @@
-from .de_giro_wrapper import *
-from .currency import *
+from .de_giro_wrapper import DeGiroWrapper, ProductInfo, Transaction
+from .currency import Currency
+
+__all__ = ["Currency", "DeGiroWrapper", "ProductInfo", "Transaction"]
